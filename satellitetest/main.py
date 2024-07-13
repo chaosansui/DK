506d42a2d5c489ddb04865ea6a65afa588935e07
Concealer <zzc15560846421@163.com>
2024-07-13 16:40:02 +0800

    Merge remote-tracking branch 'origin/main'

--- satellitetest/main.py
+++ satellitetest/main.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot as plt
 from poliastro.bodies import Earth
 from astropy.time import Time, TimeDelta
 import numpy as np
-from satellite_env import SatelliteEnv
 
 from orbits.orbit_red import create_satellite_red_orbit
 from interface.orbits_untils import OrbitalElementsInterface
@@ -16,63 +15,42 @@ from orbits.orbit_blue import generate_blue_orbit
 from satellites.satellite_blue import satellite_blue_run
 from interface.orbits_maneuver import OrbitalManeuverInterface
 from orbits.orbital_calculations import calculate_updated_orbit
-from train import test
 
+specific_time = Time("2022-01-01T00:00:00", scale="utc")
 
 def create():
     # 创建一个红色方固定的轨道
     red_orb = create_satellite_red_orbit()
     print(red_orb)
 
-    # 创建字典 区分轨道
-    satellite_orbital_elements = {
-        "satellite_redacc": OrbitalElementsInterface(),
-        "satellite_blue": OrbitalElementsInterface()
-    }
+    # 创建轨道要素接口实例
+    orbital_elements = OrbitalElementsInterface()
 
-    specific_time = Time("2022-01-01T00:00:00", scale="utc")
+    # 设置轨道要素
+    orbital_elements.set_semi_major_axis(6678 * u.km)
+    orbital_elements.set_eccentricity(0.01 * u.one)
+    orbital_elements.set_inclination(57 * u.deg)
+    orbital_elements.set_raan(0 * u.deg)
+    orbital_elements.set_arg_periapsis(0 * u.deg)
+    orbital_elements.set_true_anomaly(0 * u.deg)
+    orbital_elements.set_specific_time(specific_time)
+    # 创建第二个轨道要素接口实例
+    orbital_elements_2 = OrbitalElementsInterface()
 
-    # 设置蓝色卫星的轨道参数
-    satellite_orbital_elements["satellite_blue"].set_semi_major_axis((300 * u.km + 1000 * u.km) / 2)
-    satellite_orbital_elements["satellite_blue"].set_eccentricity(0.01 * u.one)
-    satellite_orbital_elements["satellite_blue"].set_inclination(57 * u.deg)
-    satellite_orbital_elements["satellite_blue"].set_raan(0 * u.deg)
-    satellite_orbital_elements["satellite_blue"].set_arg_periapsis(0 * u.deg)
-    satellite_orbital_elements["satellite_blue"].set_true_anomaly(0 * u.deg)
-    OrbitalElementsInterface.set_specific_time(specific_time)
+    # 设置第二个轨道的要素
+    orbital_elements_2.set_semi_major_axis(7000 * u.km)  # 更高的LEO轨道
+    orbital_elements_2.set_eccentricity(0.02 * u.one)  # 略微增加偏心率
+    orbital_elements_2.set_inclination(90 * u.deg)  # 极地轨道
+    orbital_elements_2.set_raan(0 * u.deg)
+    orbital_elements_2.set_arg_periapsis(0 * u.deg)
+    orbital_elements_2.set_true_anomaly(0 * u.deg)
+    orbital_elements_2.set_specific_time(specific_time)  # 相同的时间点
 
-    # 使用 generate_orbit 函数创建轨道
-    redacc_orbit = generate_redacc_orbit(
-        satellite_orbital_elements["satellite_blue"].get_semi_major_axis(),
-        satellite_orbital_elements["satellite_blue"].get_eccentricity(),
-        satellite_orbital_elements["satellite_blue"].get_inclination(),
-        satellite_orbital_elements["satellite_blue"].get_raan(),
-        satellite_orbital_elements["satellite_blue"].get_arg_periapsis(),
-        satellite_orbital_elements["satellite_blue"].get_true_anomaly(),
-        specific_time
-    )
+
+    # 生成轨道
+    blue_orbit = generate_blue_orbit(orbital_elements)
+    redacc_orbit= generate_redacc_orbit(orbital_elements_2)
     print(redacc_orbit)
-
-    # 设置红色加速卫星的轨道参数
-    satellite_orbital_elements["satellite_redacc"].set_semi_major_axis((350 * u.km + 800 * u.km) / 2)
-    satellite_orbital_elements["satellite_redacc"].set_eccentricity(0.01 * u.one)
-    satellite_orbital_elements["satellite_redacc"].set_inclination(57 * u.deg)
-    satellite_orbital_elements["satellite_redacc"].set_raan(0 * u.deg)
-    satellite_orbital_elements["satellite_redacc"].set_arg_periapsis(0 * u.deg)
-    satellite_orbital_elements["satellite_redacc"].set_true_anomaly(0 * u.deg)
-    OrbitalElementsInterface.set_specific_time(specific_time)
-
-    # 使用 generate_orbit 函数创建轨道
-    blue_orbit = generate_blue_orbit(
-        satellite_orbital_elements["satellite_redacc"].get_semi_major_axis(),
-        satellite_orbital_elements["satellite_redacc"].get_eccentricity(),
-        satellite_orbital_elements["satellite_redacc"].get_inclination(),
-        satellite_orbital_elements["satellite_redacc"].get_raan(),
-        satellite_orbital_elements["satellite_redacc"].get_arg_periapsis(),
-        satellite_orbital_elements["satellite_redacc"].get_true_anomaly(),
-        specific_time
-    )
-    print(blue_orbit)
 
     # 变轨机动
     # 设置机动时间和 delta_v
@@ -81,17 +59,14 @@ def create():
 
     # 计算更新后的轨道
     updated_orbit = calculate_updated_orbit(blue_orbit, maneuver_time, delta_v)
+    return red_orb,blue_orbit,redacc_orbit
 
-    return {
-        'red':red_orb,
-        'redacc':redacc_orbit,
-        'blue':blue_orbit
-    }
+
 
 
 
 def main():
     create()
-    test()
 if __name__ == "__main__":
     main()
+
--- satellitetest/orbits/orbit_redacc.py
+++ satellitetest/orbits/orbit_redacc.py
@@ -2,7 +2,27 @@ from astropy import units as u
 from astropy.time import Time
 from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
+from satellitetest.interface.orbits_untils import OrbitalElementsInterface
 
-def generate_redacc_orbit(semi_major_axis: u.Quantity, eccentricity: u.Quantity, inclination: u.Quantity,
-                   raan: u.Quantity, arg_periapsis: u.Quantity, true_anomaly: u.Quantity, epoch: Time) -> Orbit:
-    return Orbit.from_classical(Earth, semi_major_axis, eccentricity, inclination, raan, arg_periapsis, true_anomaly, epoch=epoch)
+
+def generate_redacc_orbit(orbital_elements: OrbitalElementsInterface) -> Orbit:
+   # 确保必要的轨道元素已经被设置
+   if (orbital_elements.get_semi_major_axis().value == 0 or
+           orbital_elements.get_inclination().value == 0 or
+           orbital_elements.get_specific_time() is None):
+      raise ValueError("Semi-major axis, inclination, and specific time must be set.")
+
+   # 获取轨道元素
+   semi_major_axis = orbital_elements.get_semi_major_axis()
+   eccentricity = orbital_elements.get_eccentricity()
+   inclination = orbital_elements.get_inclination()
+   raan = orbital_elements.get_raan()
+   arg_periapsis = orbital_elements.get_arg_periapsis()
+   true_anomaly = orbital_elements.get_true_anomaly()
+   epoch = orbital_elements.get_specific_time()
+
+   # 创建轨道
+   redacc_orbit = Orbit.from_classical(Earth, semi_major_axis, eccentricity, inclination, raan, arg_periapsis,
+                                     true_anomaly, epoch=epoch)
+
+   return redacc_orbit
